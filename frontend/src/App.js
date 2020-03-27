d127e3e9f6a10fe35b53c577f9af83d5e192e5ab
Larissa Araújo <lariaraujo201@gmail.com>
2020-03-27 16:16:56 -0300

    Configurado eslint e prettier.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Routes from './routes';
 
-import './global.css'
+import './global.css';
 
 function App() {
-  return (
-    <Routes />
-  );
+  return <Routes />;
 }
 
 export default App;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -12,12 +12,12 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
-      
+
         <Route path="/profile" component={Profile} />
         <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
